8bc468940ededf707c85ee00386cc893ca4e40f0
Sophia <sophie@soap-phia.dev>
2025-12-06 12:36:20 -0500

    :3

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,14 @@ import { baremuxPath } from '@mercuryworkshop/bare-mux/node';
 import { scramjetPath } from '@mercuryworkshop/scramjet/path';
 import { defineConfig } from "astro/config";
 import { viteStaticCopy } from "vite-plugin-static-copy";
-import { readFileSync } from 'fs';
-import { join } from 'path';
+
 
 const staticBuild = process.env.staticBuild === "static";
 const output = staticBuild ? "static" : "server";
 let replacement;
 import packagejson from './package.json';
-
-if (staticBuild) replacement = JSON.stringify(`https://cdn.jsdelivr.net/npm/@nuggetscorporation/nuggets@${packagejson.version}/dist`);
-else replacement = JSON.stringify('');
-
+if (staticBuild) replacement=JSON.stringify(`https://cdn.jsdelivr.net/npm/@nuggetscorporation/nuggets@${packagejson.version}/dist`);
+else replacement=JSON.stringify('');
 export default defineConfig({
     output: output,
     adapter: staticBuild ? undefined : node({mode: "middleware"}),
@@ -25,24 +22,13 @@ export default defineConfig({
         assets: "_astro",
     },
     vite: {
-        define: { 
-            'replacement': replacement,
-        },
+        define: { 'replacement': replacement },
         plugins: [viteStaticCopy({targets:[
             {src: `${scramjetPath}/**/*`.replace(/\\/g, "/"), dest: "sj"},
             {src: `${uvPath}/**/*`.replace(/\\/g, "/"), dest: "uv"},
             {src: `${epoxyPath}/**/*`.replace(/\\/g, "/"), dest: "epoxy"},
             {src: `${libcurlPath}/**/*`.replace(/\\/g, "/"), dest: "libcurl"},
-            {
-                src: `${baremuxPath}/**/*`.replace(/\\/g, "/"), 
-                dest: "baremux",
-                transform: (content, filename) => {
-                    if (filename.endsWith('index.js')) {
-                        return content.toString().replace(/!e\.startsWith\("\/"\)&&/g, '');
-                    }
-                    return content;
-                }
-            },
+            {src: `${baremuxPath}/**/*`.replace(/\\/g, "/"), dest: "baremux"},
             {src: `${baremuxPath}/worker.js`.replace(/\\/g, "/"), dest: ".", rename: "bareworker.js"},
         ]})],
         server: {
